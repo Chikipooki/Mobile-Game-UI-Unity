d03e44dba4967d1f0d1e792f2a7fff9ea45adfba
Alexey Taradoyna <AltarNatsev@yandex.ru>
2023-12-25 00:44:42 +0700

    feat: Created SoundManager

---
+++ Assets/Scripts/Core/SoundManager.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager instance { get; private set; }
+    private AudioSource source;
+
+    private void Awake()
+    {
+        instance = this;
+        source = GetComponent<AudioSource>();
+    }
+
+    public void PlaySound(AudioClip _sound)
+    {
+        source.PlayOneShot(_sound);
+    }
+}
